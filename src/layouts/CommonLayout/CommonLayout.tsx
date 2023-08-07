8d5d436aa8f6d2aa1d297c3e14c929727cb1d833
Arya Ramadika <79522791+aryaramadika@users.noreply.github.com>
2023-08-07 13:03:47 +0800

    feat(tremor): setup tremor skeleton

--- src/layouts/CommonLayout/CommonLayout.tsx
+++ src/layouts/CommonLayout/CommonLayout.tsx
@@ -9,21 +9,8 @@ interface CommonLayoutProps {
 
 export const CommonLayout: FC<CommonLayoutProps> = ({ children }) => {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>{children}</main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+    <div className="">
+      <main className="">{children}</main>
     </div>
   );
 };
